2ba4dc56cfd7eeb2c8687f5e704aef8bd39e34c6
brunnerj <brunnerj16@gmail.com>
2017-01-22 01:35:23 -0800

    Whoops. Forgot to commit change to slack module that enables posting to an aribitrary channel.

--- watchers/slack.py
+++ watchers/slack.py
@@ -7,55 +7,56 @@ import urllib
 import urllib2
 
 
-def postMessage(text):
+def postMessage(text, channel):
   """Posts a text message to the Slack team."""
   url = "https://slack.com/api/chat.postMessage"
-  fields = {"text": text, "token": tokens.filefairy, "channel": "live-sim-discussion",
+  fields = {"text": text, "token": tokens.filefairy, "channel": channel,
             "link_names": "brunnerj,everyone", "as_user": "true"}
   full = "{0}?{1}".format(url, urllib.urlencode(fields))
   urllib2.urlopen(full)
 
 
-def upload(filename):
+def upload(filename, channel):
   """Uploads a file to the Slack team."""
   fi = "file=@{0}".format(filename)
+  ch = "channels=#{0}".format(channel)
   token = "token={0}".format(tokens.filefairy)
   url = "https://slack.com/api/files.upload"
 
   with open(os.devnull, "wb") as f:
-    subprocess.call(["curl", "-F", fi, "-F", "channels=#testing",
-                     "-F", token, url], stderr=f, stdout=f)
+    subprocess.call(["curl", "-F", fi, "-F", ch, "-F",
+                     token, url], stderr=f, stdout=f)
   # subprocess.call(["rm", self.filename])
 
 icons = {
-  "ATL": ":braves:",
-  "ARI": ":dbacks:",
-  "BAL": ":orioles:",
-  "BOS": ":redsox:",
-  "CHC": ":cubs:",
-  "CIN": ":reds:",
-  "CLE": ":indians:",
-  "COL": ":rockies:",
-  "CWS": ":whitesox:",
-  "DET": ":tigers:",
-  "HOU": ":astros:",
-  "KC": ":royals:",
-  "LAA": ":angels:",
-  "LAD": ":dodgers:",
-  "MIA": ":marlins:",
-  "MIL": ":brewers:",
-  "MIN": ":twins:",
-  "NYM": ":mets:",
-  "NYY": ":yankees:",
-  "OAK": ":athletics:",
-  "PHI": ":phillies:",
-  "PIT": ":pirates:",
-  "SD": ":padres:",
-  "SF": ":giants:",
-  "SEA": ":mariners:",
-  "STL": ":cardinals:",
-  "TB": ":rays:",
-  "TEX": ":rangers:",
-  "TOR": ":jays:",
-  "WAS": ":nationals:",
+    "ATL": ":braves:",
+    "ARI": ":dbacks:",
+    "BAL": ":orioles:",
+    "BOS": ":redsox:",
+    "CHC": ":cubs:",
+    "CIN": ":reds:",
+    "CLE": ":indians:",
+    "COL": ":rockies:",
+    "CWS": ":whitesox:",
+    "DET": ":tigers:",
+    "HOU": ":astros:",
+    "KC": ":royals:",
+    "LAA": ":angels:",
+    "LAD": ":dodgers:",
+    "MIA": ":marlins:",
+    "MIL": ":brewers:",
+    "MIN": ":twins:",
+    "NYM": ":mets:",
+    "NYY": ":yankees:",
+    "OAK": ":athletics:",
+    "PHI": ":phillies:",
+    "PIT": ":pirates:",
+    "SD": ":padres:",
+    "SF": ":giants:",
+    "SEA": ":mariners:",
+    "STL": ":cardinals:",
+    "TB": ":rays:",
+    "TEX": ":rangers:",
+    "TOR": ":jays:",
+    "WAS": ":nationals:",
 }
