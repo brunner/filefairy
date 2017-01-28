ec4b4223e117c731c53f8089276a790ad88f9c6e
brunner <brunnerj16@gmail.com>
2017-01-28 13:31:18 -0800

    Increase simwatcher timeout.

--- watchers/sim_watcher.py
+++ watchers/sim_watcher.py
@@ -201,7 +201,7 @@ class SimWatcher(object):
     """Returns a tuple of values, in seconds, for the watchLiveSim timer."""
     return [
         5,      # 5 seconds, to sleep between consecutive page checks.
-        180,    # 3 minutes, after which (if the page is currently static but
+        360,    # 6 minutes, after which (if the page is currently static but
                 #     had changed previously) the sim is presumed to be over
                 #     and the last screenshot can be uploaded to Slack.
         18000,  # 5 hours, to wait for an initial page change, before timing
