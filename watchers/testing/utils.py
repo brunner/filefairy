6cee3a8e40d05ea31e35540d5cde1d8ae73a3197
brunnerj <brunnerj16@gmail.com>
2017-04-29 21:35:24 -0700

    Remove obsolete utils.

--- watchers/testing/utils.py
+++ watchers/testing/utils.py
@@ -13,31 +13,3 @@ def assertNotEquals(actual, expected):
   if actual == expected:
     raise AssertionError(
         "Expected {0} to not match {1}, but it did.".format(expected, actual))
-
-def assertContains(array, element):
-  if element not in array:
-    raise AssertionError(
-        "Expected {0} to contain {1}, but it didn't.".format(array, element))
-
-
-def getExportUrls():
-  path = "http://brunnerj.com/orangeandblueleague/"
-  files = [
-      "export_01142017_1.html",         # 0. Initial exports page.
-      "export_01142017_2.html",         # 1. League file date has not changed.
-      "export_01172017_1.html",         # 2. League file date has changed.
-  ]
-  return [os.path.join(path, fi) for fi in files]
-
-
-def getSimUrls():
-  path = "http://brunnerj.com/orangeandblueleague/"
-  files = [
-      "sim_09052018_1.html",            # 0. Initial sim page.
-      "sim_09052018_2.html",            # 1. Same date. No new final games.
-      "sim_09052018_3.html",            # 2. Same date. One new final game.
-      "sim_09092018_1.html",            # 3. Different date, partially loaded.
-      "sim_09092018_2.html",            # 4. Fully loaded.
-      "sim_09092018_3.html",            # 5. Partially loaded again.
-      ]
-  return [os.path.join(path, fi) for fi in files]
