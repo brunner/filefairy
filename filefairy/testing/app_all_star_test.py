18ce2f41fef4ce961c7537029f030d72902a43ad
brunnerj <brunnerj16@gmail.com>
2017-11-30 01:29:36 -0800

    Add hypothetical all star test.

---
+++ filefairy/testing/app_all_star_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import threading
+import time
+
+from app_test import AppTest
+from slack_api import chat_post_message
+from utils import assert_equals
+
+
+inpt = ['07/27/2021 MAJOR LEAGUE BASEBALL Final Scores\n' +
+        '*<https://game_box_0.html|AL 4, NL 3>*',
+        '```MAJOR LEAGUE BASEBALL Live Table - 03/21/2021\n' +
+        'AL                              1\nNL                              0````']
+
+outpt = '07/27/2021\n' + \
+        '<https://game_box_0.html|AL 4, NL 3>'
+
+
+def test():
+  appTest = AppTest(standings_in='data/standings_in.txt')
+  appTest.setup()
+
+  t1 = threading.Thread(target=appTest.listen)
+  t1.start()
+  time.sleep(6)
+
+  for i in inpt:
+    chat_post_message('testing', i)
+  time.sleep(2)
+
+  assert_equals(appTest.process_final_scores(), outpt)
+  assert_equals(appTest.process_records(), '')
+
+  appTest.handle_close()
+  t1.join()
+
+
+if __name__ == '__main__':
+  test()
