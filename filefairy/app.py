ec3f9259f6c99a1066abe83965ed5c40672f8a0e
brunnerj <brunnerj16@gmail.com>
2017-12-22 13:12:39 -0800

    Remove extra paren

--- filefairy/app.py
+++ filefairy/app.py
@@ -231,7 +231,7 @@ class App(object):
       chat_post_message(self.get_testing_name(), deb.strip('\n'))
     if text == 'Run git reset.':
       deb = subprocess.check_output(
-          ['git', 'reset', '--hard'], stdout=open(os.devnull, 'w'), stderr=subprocess.STDOUT))
+          ['git', 'reset', '--hard'], stdout=open(os.devnull, 'w'), stderr=subprocess.STDOUT)
       chat_post_message(self.get_testing_name(), deb.strip('\n'))
     elif text == 'Run setup.':
       self.setup()
