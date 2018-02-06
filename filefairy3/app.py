7f2883ff771afe90bfaddb53d474ce67233e8be2
brunnerj <brunnerj16@gmail.com>
2018-02-05 21:01:32 -0800

    Make app messageable.

--- filefairy3/app.py
+++ filefairy3/app.py
@@ -1,34 +1,45 @@
 #!/usr/bin/env python
 
 import json
+import os
+import sys
 import threading
 import time
 import websocket
 
+from apis.messageable.messageable_api import MessageableApi
 from plugins.exports.exports_plugin import ExportsPlugin
 from plugins.git.git_plugin import GitPlugin
 from plugins.league_file.league_file_plugin import LeagueFilePlugin
 from utils.slack.slack_util import rtm_connect
 
 
-class App(object):
+class App(MessageableApi):
     def __init__(self):
+        super(App, self).__init__()
+
+        self.keep_running = True
         self.lock = threading.Lock()
-        self.plugins = []
+        self.sleep = 2
         self.ws = None
 
-    def setup(self):
-        self.sleep = 120
+        self.plugins = []
 
+    def _on_message_internal(self, obj):
+        pass
+
+    def _setup(self):
         self.plugins.append(ExportsPlugin())
         self.plugins.append(GitPlugin())
         self.plugins.append(LeagueFilePlugin())
 
-    def connect(self):
+    def _connect(self):
         def _on_message(ws, message):
             self.lock.acquire()
 
             obj = json.loads(message)
+
+            self._on_message(obj)
             for p in self.plugins:
                 p._on_message(obj)
 
@@ -42,13 +53,13 @@ class App(object):
             t.daemon = True
             t.start()
 
-    def run(self):
-        while True:
+    def _start(self):
+        while self.keep_running:
             self.lock.acquire()
 
             if self.ws and not self.ws.sock:
                 self.ws.close()
-                self.connect()
+                self._connect()
 
             for p in self.plugins:
                 p._run()
@@ -56,10 +67,18 @@ class App(object):
             self.lock.release()
             time.sleep(self.sleep)
 
+        self.ws.close()
+
+    def reboot(self, *args):
+        os.execv(sys.executable, ['python'] + sys.argv)
+
+    def shutdown(self, *args):
+        self.keep_running = False
+
 
 if __name__ == '__main__':
     app = App()
 
-    app.setup()
-    app.connect()
-    app.run()
+    app._setup()
+    app._connect()
+    app._start()
