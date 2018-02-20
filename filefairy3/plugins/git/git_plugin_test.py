4cef5b54570d5cffeb817357c29d93cc7c4218b4
brunnerj <brunnerj16@gmail.com>
2018-02-20 08:20:15 -0800

    Update test input after messageable api change.

--- filefairy3/plugins/git/git_plugin_test.py
+++ filefairy3/plugins/git/git_plugin_test.py
@@ -12,12 +12,12 @@ class GitPluginTest(unittest.TestCase):
     def test_add(self, mock_check, mock_log):
         mock_check.return_value = ''
         mock_log.return_value = ''
-        data = {'a': '', 'v': True}
+        data = {'a1': '', 'v': True}
         plugin = GitPlugin()
         plugin.add(**data)
         mock_check.assert_called_once_with(['git', 'add', '.'])
         mock_log.assert_called_once_with(plugin._name(), **{
-            'a': '',
+            'a1': '',
             'r': '',
             's': 'Call completed.',
             'v': True
@@ -28,14 +28,14 @@ class GitPluginTest(unittest.TestCase):
     def test_commit(self, mock_check, mock_log):
         mock_check.return_value = '[master 0abcd0a] Auto...\n1 files\n'
         mock_log.return_value = '[master 0abcd0a] Auto...\n1 files'
-        data = {'a': '', 'v': True}
+        data = {'a1': '', 'v': True}
         plugin = GitPlugin()
         plugin.commit(**data)
         mock_check.assert_called_once_with(
             ['git', 'commit', '-m', 'Automated data push.'])
         mock_log.assert_called_once_with(
             plugin._name(), **{
-                'a': '',
+                'a1': '',
                 'r': '[master 0abcd0a] Auto...\n1 files',
                 's': 'Call completed.',
                 'v': True
@@ -46,13 +46,13 @@ class GitPluginTest(unittest.TestCase):
     def test_pull(self, mock_check, mock_log):
         mock_check.return_value = 'remote: Counting...\nUnpacking...\n'
         mock_log.return_value = 'remote: Counting...\nUnpacking...'
-        data = {'a': '', 'v': True}
+        data = {'a1': '', 'v': True}
         plugin = GitPlugin()
         plugin.pull(**data)
         mock_check.assert_called_once_with(['git', 'pull'])
         mock_log.assert_called_once_with(
             plugin._name(), **{
-                'a': '',
+                'a1': '',
                 'r': 'remote: Counting...\nUnpacking...',
                 's': 'Call completed.',
                 'v': True
@@ -63,13 +63,13 @@ class GitPluginTest(unittest.TestCase):
     def test_push(self, mock_check, mock_log):
         mock_check.return_value = 'Counting...\nCompressing...\n'
         mock_log.return_value = 'Counting...\nCompressing...'
-        data = {'a': '', 'v': True}
+        data = {'a1': '', 'v': True}
         plugin = GitPlugin()
         plugin.push(**data)
         mock_check.assert_called_once_with(['git', 'push', 'origin', 'master'])
         mock_log.assert_called_once_with(
             plugin._name(), **{
-                'a': '',
+                'a1': '',
                 'r': 'Counting...\nCompressing...',
                 's': 'Call completed.',
                 'v': True
@@ -80,13 +80,13 @@ class GitPluginTest(unittest.TestCase):
     def test_reset(self, mock_check, mock_log):
         mock_check.return_value = ''
         mock_log.return_value = ''
-        data = {'a': '', 'v': True}
+        data = {'a1': '', 'v': True}
         plugin = GitPlugin()
         plugin.reset(**data)
         mock_check.assert_called_once_with(['git', 'reset', '--hard'])
         mock_log.assert_called_once_with(
             plugin._name(), **{
-                'a': '',
+                'a1': '',
                 'r': '',
                 's': 'Call completed.',
                 'v': True
@@ -97,13 +97,13 @@ class GitPluginTest(unittest.TestCase):
     def test_status(self, mock_check, mock_log):
         mock_check.return_value = 'On branch master\nYour branch...\n'
         mock_log.return_value = 'On branch master\nYour branch...'
-        data = {'a': '', 'v': True}
+        data = {'a1': '', 'v': True}
         plugin = GitPlugin()
         plugin.status(**data)
         mock_check.assert_called_once_with(['git', 'status'])
         mock_log.assert_called_once_with(
             plugin._name(), **{
-                'a': '',
+                'a1': '',
                 'r': 'On branch master\nYour branch...',
                 's': 'Call completed.',
                 'v': True
