36cdf42a25c5730b05ef321888abea3c54602856
brunnerj <brunnerj16@gmail.com>
2018-03-14 01:03:35 -0700

    Define utf-8 encoding.

--- filefairy3/utils/ago/ago_util.py
+++ filefairy3/utils/ago/ago_util.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 
 def delta(then, now, fmt='{} ago'):
--- filefairy3/utils/component/component_util.py
+++ filefairy3/utils/component/component_util.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 
 def card(href='', title='', info='', table=[], ts='', success='', danger=''):
