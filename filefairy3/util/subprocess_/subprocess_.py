866f20c7860a1733f49fab6861de3dbffdea5869
brunnerj <brunnerj16@gmail.com>
2018-05-21 21:16:34 -0700

    Update subprocess_ after python3.

--- filefairy3/util/subprocess_/subprocess_.py
+++ filefairy3/util/subprocess_/subprocess_.py
@@ -2,41 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import subprocess
-import threading
 
 
-class Command(object):
-    def __init__(self, cmd):
-        self.output = {'ok': True}
-        self.proc = subprocess.Popen(
-            cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def check_output(cmd, timeout=None):
+    output = {'ok': True}
+    try:
+        proc = subprocess.run(
+            cmd,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            timeout=timeout,
+            check=True)
+        if isinstance(proc, subprocess.CompletedProcess):
+            output.update({'output': proc.stdout.decode('utf-8')})
+    except subprocess.SubprocessError as e:
+        output.update({'ok': False, 'output': str(e)})
 
-    def run(self, timeout):
-        try:
-            t = threading.Thread(target=self._target)
-            t.start()
-
-            if timeout:
-                t.join(timeout)
-                if t.is_alive():
-                    self.proc.terminate()
-                    self.output.update({'ok': False, 'error': 'timeout'})
-
-            t.join()
-        except Exception as e:
-            self.output.update({
-                'ok': False,
-                'error': 'exception',
-                'exception': str(e)
-            })
-
-        return self.output
-
-    def _target(self):
-        stdout, _ = self.proc.communicate()
-        if stdout:
-            self.output.update({'output': stdout.decode('utf-8')})
-
-
-def check_output(cmd, timeout=0):
-    return Command(cmd).run(timeout)
+    return output
--- filefairy3/util/subprocess_/subprocess_test.py
+++ filefairy3/util/subprocess_/subprocess_test.py
@@ -10,117 +10,45 @@ import unittest.mock as mock
 
 _path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(re.sub(r'/util/subprocess_', '', _path))
-from util.subprocess_.subprocess_ import Command  # noqa
 from util.subprocess_.subprocess_ import check_output  # noqa
 
 
 class SubprocessTest(unittest.TestCase):
-    @mock.patch('util.subprocess_.subprocess_.threading.Thread')
-    @mock.patch.object(Command, '_target')
-    @mock.patch('util.subprocess_.subprocess_.subprocess.Popen')
-    def test_run__ok(self, mock_popen, mock_target, mock_thread):
-        mock_proc = mock.Mock()
-        mock_popen.return_value = mock_proc
-        mock_t = mock.Mock()
-        mock_t.is_alive.return_value = False
-        mock_t.start.side_effect = mock_target
-        mock_thread.return_value = mock_t
+    @mock.patch('util.subprocess_.subprocess_.subprocess.run')
+    def test_run__ok(self, mock_run):
+        cmd = ['cmd', 'foo', 'bar']
+        t = 10
+        mock_run.return_value = subprocess.CompletedProcess(
+            args=cmd, returncode=0, stdout=b'ret', stderr=None)
 
-        timeout = 10
-        command = Command(['cmd', 'foo', 'bar'])
-
-        def fake_target(*args, **kwargs):
-            command.output.update({'output': 'ret'})
-
-        mock_target.side_effect = fake_target
-
-        actual = command.run(timeout)
+        actual = check_output(cmd, timeout=t)
         expected = {'ok': True, 'output': 'ret'}
         self.assertEqual(actual, expected)
 
-        mock_popen.assert_called_once_with(
-            ['cmd', 'foo', 'bar'],
+        mock_run.assert_called_once_with(
+            cmd,
             stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
-        mock_proc.terminate.assert_not_called()
-        mock_t.start.assert_called_once_with()
-        mock_t.join.has_calls([mock.call(timeout), mock.call()])
-        mock_t.is_alive.assert_called_once_with()
-        mock_target.assert_called_once_with()
-        mock_thread.assert_called_once_with(target=mock_target)
+            stderr=subprocess.PIPE,
+            timeout=t,
+            check=True)
 
-    @mock.patch('util.subprocess_.subprocess_.threading.Thread')
-    @mock.patch.object(Command, '_target')
-    @mock.patch('util.subprocess_.subprocess_.subprocess.Popen')
-    def test_run__exception(self, mock_popen, mock_target, mock_thread):
-        mock_proc = mock.Mock()
-        mock_popen.return_value = mock_proc
-        mock_t = mock.Mock()
-        mock_t.is_alive.return_value = False
-        mock_t.start.side_effect = mock_target
-        mock_target.side_effect = Exception('Error.')
-        mock_thread.return_value = mock_t
+    @mock.patch('util.subprocess_.subprocess_.subprocess.run')
+    def test_run__exception(self, mock_run):
+        cmd = ['cmd', 'foo', 'bar']
+        t = 10
+        mock_run.side_effect = subprocess.TimeoutExpired(cmd, t)
 
-        timeout = 10
-        command = Command(['cmd', 'foo', 'bar'])
-        actual = command.run(timeout)
-        expected = {'ok': False, 'error': 'exception', 'exception': 'Error.'}
+        actual = check_output(cmd, timeout=t)
+        ret = 'Command \'{0}\' timed out after {1} seconds'.format(cmd, t)
+        expected = {'ok': False, 'output': ret}
         self.assertEqual(actual, expected)
 
-        mock_popen.assert_called_once_with(
-            ['cmd', 'foo', 'bar'],
+        mock_run.assert_called_once_with(
+            cmd,
             stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
-        mock_proc.terminate.assert_not_called()
-        mock_target.assert_called_once_with()
-        mock_t.start.assert_called_once_with()
-        mock_t.join.assert_not_called()
-        mock_t.is_alive.assert_not_called()
-        mock_thread.assert_called_once_with(target=mock_target)
-
-    @mock.patch('util.subprocess_.subprocess_.threading.Thread')
-    @mock.patch.object(Command, '_target')
-    @mock.patch('util.subprocess_.subprocess_.subprocess.Popen')
-    def test_run__timeout(self, mock_popen, mock_target, mock_thread):
-        mock_proc = mock.Mock()
-        mock_popen.return_value = mock_proc
-        mock_t = mock.Mock()
-        mock_t.is_alive.return_value = True
-        mock_t.start.side_effect = mock_target
-        mock_thread.return_value = mock_t
-
-        timeout = 10
-        command = Command(['cmd', 'foo', 'bar'])
-        actual = command.run(timeout)
-        expected = {'ok': False, 'error': 'timeout'}
-        self.assertEqual(actual, expected)
-
-        mock_popen.assert_called_once_with(
-            ['cmd', 'foo', 'bar'],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
-        mock_proc.terminate.assert_called_once_with()
-        mock_t.start.assert_called_once_with()
-        mock_t.join.has_calls([mock.call(timeout), mock.call()])
-        mock_t.is_alive.assert_called_once_with()
-        mock_target.assert_called_once_with()
-        mock_thread.assert_called_once_with(target=mock_target)
-
-    @mock.patch('util.subprocess_.subprocess_.subprocess.Popen')
-    def test_target(self, mock_popen):
-        mock_proc = mock.Mock()
-        mock_proc.communicate.return_value = (b'ret', b'')
-        mock_popen.return_value = mock_proc
-
-        command = Command(['cmd', 'foo', 'bar'])
-        command._target()
-        self.assertEqual(command.output, {'ok': True, 'output': 'ret'})
-
-        mock_popen.assert_called_once_with(
-            ['cmd', 'foo', 'bar'],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
-        mock_proc.communicate.assert_called_once_with()
+            stderr=subprocess.PIPE,
+            timeout=t,
+            check=True)
 
 
 if __name__ == '__main__':
