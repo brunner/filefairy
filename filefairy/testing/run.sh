7bd11d9d72c2e18d25312713a6fa63bcfa919ee8
brunnerj <brunnerj16@gmail.com>
2018-05-26 14:32:55 -0700

    Bye bye old filefairy.

--- filefairy/testing/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-for py_file in $(find $DIR -name *$1.py)
-do
-  python $py_file
-done
--- filefairy/testing/utils.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-
-def assert_equals(actual, expected):
-  if actual != expected:
-    raise AssertionError("Expected {0} to match {1}, but it didn't.".format(expected, actual))
