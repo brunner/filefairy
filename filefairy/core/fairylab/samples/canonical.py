4cb017e687bb5fa12867d52a3e463d88f074391e
brunnerj <brunnerj16@gmail.com>
2018-05-29 23:45:25 -0700

    Standardize sample format.

--- filefairy/core/fairylab/samples/canonical.py
+++ filefairy/core/fairylab/samples/canonical.py
@@ -13,53 +13,51 @@ from util.component.component import card  # noqa
 _breadcrumbs = [{'href': '', 'name': 'Home'}]
 
 _browsable = [
-        card(
-            href='/fairylab/bar/',
-            title='bar',
-            info='Description of bar.',
-            ts='10m ago',
-        ),
-        card(
-            href='/fairylab/baz/',
-            title='baz',
-            info='Description of baz.',
-            ts='1h ago',
-            danger='failed'),
-        card(
-            href='/fairylab/foo/',
-            title='foo',
-            info='Description of foo.',
-            ts='2m ago',
-        )
-    ]
+    card(
+        href='/fairylab/bar/',
+        title='bar',
+        info='Description of bar.',
+        ts='10m ago',
+    ),
+    card(
+        href='/fairylab/baz/',
+        title='baz',
+        info='Description of baz.',
+        ts='1h ago',
+        danger='failed'),
+    card(
+        href='/fairylab/foo/',
+        title='foo',
+        info='Description of foo.',
+        ts='2m ago',
+    )
+]
 _internal = [
-        card(
-            title='quux',
-            info='Description of quux.',
-            ts='15m ago',
-            danger='failed'),
-        card(
-            title='quuz',
-            info='Description of quuz.',
-            ts='30s ago',
-            success='just now',
-        ),
-        card(
-            title='qux',
-            info='Description of qux.',
-            ts='2d ago',
-        )
-    ]
+    card(
+        title='quux',
+        info='Description of quux.',
+        ts='15m ago',
+        danger='failed'),
+    card(
+        title='quuz',
+        info='Description of quuz.',
+        ts='30s ago',
+        success='just now',
+    ),
+    card(
+        title='qux',
+        info='Description of qux.',
+        ts='2d ago',
+    )
+]
 
 subtitle = ''
 
 tmpl = 'home.html'
 
 context = {
-    'title':
-    'home',
-    'breadcrumbs':
-    _breadcrumbs,
+    'title': 'home',
+    'breadcrumbs': _breadcrumbs,
     'browsable': _browsable,
     'internal': _internal,
 }
