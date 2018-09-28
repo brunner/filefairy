05f7af089535639603a2f16f4fa6aae36fe9a534
brunnerj <brunnerj16@gmail.com>
2018-09-27 18:44:09 -0700

    Suppress 403 logging.

--- util/urllib_/urllib_.py
+++ util/urllib_/urllib_.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import urllib.error as error
 import urllib.parse as parse
 import urllib.request as request
 
@@ -13,6 +14,10 @@ def urlopen(url, params={}):
         data = parse.urlencode(params).encode('utf-8')
         with request.urlopen(url, data=data, timeout=8) as f:
             return f.read()
+    except error.HTTPError as e:
+        if e.code != 403:
+            logger_.log(logging.WARNING, 'Handled warning.', exc_info=True)
+        return b''
     except:
         logger_.log(logging.WARNING, 'Handled warning.', exc_info=True)
         return b''
