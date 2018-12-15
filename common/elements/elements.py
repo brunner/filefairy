37ef3167787c5b4e6c350deff065491861e162e1
brunnerj <brunnerj16@gmail.com>
2018-12-14 21:56:03 -0800

    Fill out remaining docstrings in elements.py.

--- common/elements/elements.py
+++ common/elements/elements.py
@@ -24,6 +24,21 @@ def card(href='',
          ts='',
          success='',
          danger=''):
+    """Builds a card element.
+
+    Args:
+        href: The optional title link.
+        title: The title text.
+        info: The optional description text.
+        code: The optional preformatted text.
+        table: The optional table content.
+        ts: The timestamp to display in the card footer.
+        success: The optional green-badge card text.
+        danger: The optional red-badge card text.
+
+    Returns:
+        The card element.
+    """
     return {
         'href': href,
         'title': title,
@@ -37,10 +52,28 @@ def card(href='',
 
 
 def cell(col=None, content=''):
+    """Builds a cell object, describing a table cell.
+
+    Args:
+        col: The col object for this particular cell.
+        content: The cell text.
+
+    Returns:
+        The cell object.
+    """
     return {'col': col, 'content': content}
 
 
 def col(clazz='', colspan=''):
+    """Builds a col object, describing a table column.
+
+    Args:
+        clazz: The CSS class(es) to apply to the column.
+        colspan: The number of columns that this column should span.
+
+    Returns:
+        The col object.
+    """
     return {'clazz': clazz, 'colspan': colspan}
 
 
@@ -65,6 +98,21 @@ def table(clazz='border mt-3',
           head=None,
           body=None,
           foot=None):
+    """Builds a table element.
+
+    Args:
+        clazz: The optional CSS class to set on the root table element.
+        id_: The optional CSS id to set on the root table element.
+        hcols: The optional list of CSS classes to apply to each head column.
+        bcols: The optional list of CSS classes to apply to each body column.
+        fcols: The optional list of CSS classes to apply to each footer column.
+        head: The optional list of head cells.
+        body: The optional list of body rows. Each row is a list of cells.
+        foot: The optional list of footer cells.
+
+    Returns:
+        The table element.
+    """
     return {
         'clazz': clazz,
         'id': id_,
