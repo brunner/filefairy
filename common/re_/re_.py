7b8bc0b6a8336be6e72897b7fd63affa37a63ad3
brunnerj <brunnerj16@gmail.com>
2019-01-26 17:30:42 -0800

    Prepare re_ module for new statslab use-cases.

--- common/re_/re_.py
+++ common/re_/re_.py
@@ -5,6 +5,12 @@
 import re
 
 
+def _strip(s):
+    if s is None:
+        return None
+    return s.strip()
+
+
 def findall(pattern, string):
     """Convenience wrapper around re.findall which returns all matches.
 
@@ -23,31 +29,44 @@ def findall(pattern, string):
     match = re.findall(pattern, string)
 
     if groups > 1:
-        return [[g.strip() for g in m] for m in match]
+        return [[_strip(g) for g in m] for m in match]
 
-    return [m.strip() for m in match]
+    return [_strip(m) for m in match]
 
 
-def find(pattern, string):
-    """Convenience wrapper around re.findall which returns the first match.
+def find(pattern, string, force_groups=False):
+    """Convenience wrapper around re.search which returns the first match.
 
     Returns the first match of pattern in string, or None if no match is found.
     The returned match is a single string (or None) if pattern has a single
     group, or a list of strings (or None) if pattern has multiple groups.
 
+    If force_groups=True and the pattern has no groups, return an empty list
+    instead of the entire matched pattern.
+
     Args:
         pattern: The regular expression pattern to match.
         string: The string to scan for the pattern.
+        force_groups: Whether to restrict the returned matches to groups only.
 
     Returns:
         The matched group(s) of pattern within string.
     """
-    match = findall(pattern, string)
-    if match:
-        return match[0]
-
     groups = re.compile(pattern).groups
-    if groups > 1:
-        return [None] * groups
+    match = re.search(pattern, string)
 
-    return None
+    if not match:
+        return [None] * groups if groups > 1 and not force_groups else None
+
+    if groups > 1:
+        try:
+            return [_strip(g) for g in match.groups()]
+        except:
+            print(pattern, string, match.groups())
+            raise
+
+    if groups == 0 and force_groups:
+        return []
+
+    m = _strip(match[groups])
+    return [m] if force_groups else m
--- common/re_/re_test.py
+++ common/re_/re_test.py
@@ -15,44 +15,94 @@ from common.re_.re_ import findall  # noqa
 
 
 class ReTest(unittest.TestCase):
-    def test_find__empty_multiple_match(self):
+    def test_find__empty_multiple_group_any(self):
         actual = find(r'(\d)\D(\d)', 'abcd1234')
         expected = [None, None]
         self.assertEqual(actual, expected)
 
-    def test_find__empty_single_match(self):
+    def test_find__empty_single_group_any(self):
         actual = find(r'\d(\D)\d', 'abcd1234')
         expected = None
         self.assertEqual(actual, expected)
 
-    def test_find__found_multiple_match(self):
+    def test_find__empty_single_match_any(self):
+        actual = find(r'\d\D\d', 'abcd1234')
+        expected = None
+        self.assertEqual(actual, expected)
+
+    def test_find__empty_multiple_group_force(self):
+        actual = find(r'(\d)\D(\d)', 'abcd1234', force_groups=True)
+        expected = None
+        self.assertEqual(actual, expected)
+
+    def test_find__empty_single_group_force(self):
+        actual = find(r'\d(\D)\d', 'abcd1234', force_groups=True)
+        expected = None
+        self.assertEqual(actual, expected)
+
+    def test_find__empty_single_match_force(self):
+        actual = find(r'\d\D\d', 'abcd1234', force_groups=True)
+        expected = None
+        self.assertEqual(actual, expected)
+
+    def test_find__found_multiple_group_any(self):
         actual = find(r'(\d)\D(\d)', 'a1b2c3d4')
         expected = ['1', '2']
         self.assertEqual(actual, expected)
 
-    def test_find__found_single_match(self):
+    def test_find__found_single_group_any(self):
         actual = find(r'\d(\D)\d', 'a1b2c3d4')
         expected = 'b'
         self.assertEqual(actual, expected)
 
-    def test_findall__empty_multiple_match(self):
+    def test_find__found_single_match_any(self):
+        actual = find(r'\d\D\d', 'a1b2c3d4')
+        expected = '1b2'
+        self.assertEqual(actual, expected)
+
+    def test_find__found_multiple_group_force(self):
+        actual = find(r'(\d)\D(\d)', 'a1b2c3d4', force_groups=True)
+        expected = ['1', '2']
+        self.assertEqual(actual, expected)
+
+    def test_find__found_single_group_force(self):
+        actual = find(r'\d(\D)\d', 'a1b2c3d4', force_groups=True)
+        expected = ['b']
+        self.assertEqual(actual, expected)
+
+    def test_find__found_single_match_force(self):
+        actual = find(r'\d\D\d', 'a1b2c3d4', force_groups=True)
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_findall__empty_multiple_group(self):
         actual = findall(r'(\d)\D(\d)', 'abcd1234')
         expected = []
         self.assertEqual(actual, expected)
 
-    def test_findall__empty_single_match(self):
+    def test_findall__empty_single_group(self):
         actual = findall(r'\d(\D)\d', 'abcd1234')
         expected = []
         self.assertEqual(actual, expected)
 
-    def test_findall__found_multiple_match(self):
+    def test_findall__empty_single_match(self):
+        actual = findall(r'\d\D\d', 'abcd1234')
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_findall__found_multiple_group(self):
         actual = findall(r'(\d)\D(\d)', 'a1b2c3d4')
         expected = [['1', '2'], ['3', '4']]
         self.assertEqual(actual, expected)
 
-    def test_findall__found_single_match(self):
+    def test_findall__found_single_group(self):
         actual = findall(r'\d(\D)\d', 'a1b2c3d4')
         expected = ['b', 'd']
+        self.assertEqual(actual, expected)
+
+    def test_findall__found_single_match(self):
+        actual = findall(r'\d\D\d', 'a1b2c3d4')
+        expected = ['1b2', '3d4']
         self.assertEqual(actual, expected)
 
 
