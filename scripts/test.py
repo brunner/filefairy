5e86faccd52e371460bb285f134faf9ef405a26a
brunnerj <brunnerj16@gmail.com>
2020-04-12 10:29:53 -0400

    Force refresh of gameday page prior to testing.

--- scripts/test.py
+++ scripts/test.py
@@ -8,6 +8,14 @@ import sys
 _path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(re.sub(r'/scripts', '', _path))
 
+from common.subprocess_.subprocess_ import check_output  # noqa
 from impl.filefairy.filefairy import main  # noqa
+
+CONTAINING_DIR = re.sub(r'/filefairy/scripts', '', _path)
+FAIRYLAB_DIR = CONTAINING_DIR + '/fairylab/static'
+GAMEDAY_DIR = os.path.join(FAIRYLAB_DIR, 'gameday')
+
+check_output(['rm', '-rf', GAMEDAY_DIR])
+check_output(['mkdir', GAMEDAY_DIR])
 
 main()
